70289c2dae88611ca476e21c938025a92ddf69a9
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 21:32:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    array.first
+  when 2
+    array.first + " and " + array.last
+  else
+    output = array.shift
+    while array.size > 1 do
+      output += ", " + array.shift
+    end
+    output += ", and " + array.shift
+    output
+  end
 end
