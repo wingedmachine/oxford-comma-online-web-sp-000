38f4bf8542bb4bc3e3765db652b2f815582c320e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 21:30:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1: array.first
+  when 2: array.first + " and " + array.last
+  else:
+    output = array.shift
+    while array.size > 2 do
+      output += ", " + array.shift
+    end
+  end
 end
