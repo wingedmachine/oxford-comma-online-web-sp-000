9c0e781655caf2d7e0032fd1eda10e207c0ce17e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 21:31:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   case array.size
-  when 1 array.first
-  when 2 array.first + " and " + array.last
+  when 1
+    array.first
+  when 2
+    array.first + " and " + array.last
   else
     output = array.shift
     while array.size > 1 do
